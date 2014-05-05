8b9200ff0d18c1c6725779a3053233c9bcf3454e
EdisonGitHub <2371285@qq.com>
2014-05-05 18:26:40 +0800

    Initial Commit

---
+++ 新浪微博练习/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  新浪微博练习
+//
+//  Created by Edison on 14-5-5.
+//  Copyright (c) 2014年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EdisonAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EdisonAppDelegate class]));
+    }
+}
